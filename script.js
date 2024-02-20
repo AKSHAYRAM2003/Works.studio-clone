4c5a24d756165e6a5801db493a072b82b8e1a08e
AKSHAYRAM2003 <ramakshay100@gmail.com>
2024-02-20 10:17:39 +0530

    complete page

--- script.js
+++ script.js
@@ -2,8 +2,37 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('[#main]'),
     smooth: true
 });
-
 // smooth
 // learp
 // data scroll speed
 // data scroll speed-direction
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+function loading(){
+    let tl = gsap.timeline()
+    tl.to("#loder #yellow1",{
+        top:"-100%",
+        delay:0.3,
+        duration:0.7,
+        ease: "power4.out",
+    })
+    tl.from("#yellow2",{
+        top:"100%",
+        delay:0.4,
+        duration:0.7,
+        ease: "power4.out",
+    },"anim")
+    tl.to("#loder h1",{
+        color:"black",
+        delay:0.3,
+        duration:0.7,
+    },"anim")
+    tl.to("#loder",{
+        display:"none",
+        delay:0.2,
+    },"anim")
+}
+
