a6066891435e003459ae6b3880d6c938b62d619a
AKSHAYRAM2003 <ramakshay100@gmail.com>
2024-04-07 00:31:24 +0530

    page1 Annimation completed

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('[#main]'),
+    el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
-// smooth
-// learp
-// data scroll speed
-// data scroll speed-direction
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
-function loading(){
-    let tl = gsap.timeline()
-    tl.to("#loder #yellow1",{
-        top:"-100%",
-        delay:0.3,
-        duration:0.7,
-        ease: "power4.out",
-    })
-    tl.from("#yellow2",{
-        top:"100%",
-        delay:0.4,
-        duration:0.7,
-        ease: "power4.out",
-    },"anim")
-    tl.to("#loder h1",{
-        color:"black",
-        delay:0.3,
-        duration:0.7,
-    },"anim")
-    tl.to("#loder",{
-        display:"none",
-        delay:0.2,
-    },"anim")
-}
+ var tl = gsap.timeline()
+gsap.to("#yellow1",{
+top:"-800",
+delay:0.5,
+duration:1.5,
+ease:"expo.out"
 
+})
+
+tl.from("#yellow2",{
+    top:"800",
+    delay:1.8,
+    duration:0.9,
+    ease:"expo.out"
+},"anim")
+ 
+tl.to("#loader h1" ,{
+    delay:1.8,
+    duration:1.2,
+    color:"black"
+},"anim")
+tl.to("#loader",{
+    
+  display:"none"
+
+})
