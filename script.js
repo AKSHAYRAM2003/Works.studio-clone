fcd35ff2685cf7e81c8174c50aeb07b10db33db9
AKSHAYRAM2003 <ramakshay100@gmail.com>
2024-04-07 23:36:25 +0530

    project UI finally completed!

--- script.js
+++ script.js
@@ -30,3 +30,35 @@ tl.to("#loader",{
   display:"none"
 
 })
+
+
+var elems =  document.querySelectorAll(".elem")
+var page2 =document.querySelector("#page2")
+elems.forEach(function(element) {
+   element.addEventListener("mouseenter",function(){
+     var bgimg =  element.getAttribute("data-img")
+     page2.style.backgroundImage =`url(${bgimg})`
+  
+   })
+});
+
+document.querySelector("#scroll-animation-3").addEventListener("click", () => {
+    scroll.scrollTo(0)
+})
+
+document.querySelector("#scroll-animation-1").addEventListener("click", () => {
+    scroll.scrollTo(page2)
+})
+
+document.querySelector("#scroll-animation-2").addEventListener("click", () => {
+    scroll.scrollTo(page3)
+})
+
+
+
+
+// this code helps to move Top to bottom or ^
+
+// document.querySelector("footer h2").addEventListener("click",()=>{
+// console.log()
+// })
